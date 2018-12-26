09f2d233628a7dbc2f789d75ffe1a8e5d8c12f9f
Leonardo Silva <leosilvadev@gmail.com>
2018-12-27 00:19:14 +0100

    setup flights api

---
+++ flights-api/src/main/java/com/github/leosilvadev/flights/FlightsApiApplication.java
@@ -0,0 +1,14 @@
+package com.github.leosilvadev.flights;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlightsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlightsApiApplication.class, args);
+	}
+
+}
+

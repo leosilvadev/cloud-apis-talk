286b14ed1412621c816d1ce3c9d2df39cbfeb28f
Leonardo Silva <leosilvadev@gmail.com>
2018-12-27 00:19:41 +0100

    setup workers api

---
+++ workers-api/src/main/java/com/github/leosilvadev/workers/WorkersApiApplication.java
@@ -0,0 +1,14 @@
+package com.github.leosilvadev.workers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkersApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkersApiApplication.class, args);
+	}
+
+}
+

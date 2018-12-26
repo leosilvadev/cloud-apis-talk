a3f24c36dd4138c5345d909e2179a0cecc209f9f
Leonardo Silva <leosilvadev@gmail.com>
2018-12-27 00:14:38 +0100

    setup airplanes-api

---
+++ airplanes-api/src/main/java/com/github/leosilvadev/airplanes/AirplanesApiApplication.java
@@ -0,0 +1,14 @@
+package com.github.leosilvadev.airplanes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirplanesApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AirplanesApiApplication.class, args);
+	}
+
+}
+
